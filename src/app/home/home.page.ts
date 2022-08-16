ce9f38e66347ec828db976c5e0b60bb52b132a39
riqueokuda <riqueokuda@gmail.com>
2022-08-16 20:23:10 -0300

    Projeto finalizado

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+
+export class HomePage {
+dado = 0;
+constructor() {}
+
+
+async sortear(){
+for(let i=0; i <10; i++){
+let numeroSorteado = Math.random();
+this.dado = Math.floor(numeroSorteado * 6 + 1 );
+await new Promise(f => setTimeout(f,100));
+    }
+  }
+}
